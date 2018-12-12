dad5e0d74ad86d43c07989e1ab0ca66d1d339211
yuanyuangong <18735182551@163.com>
2018-12-12 11:15:51 +0800

    第一次上传测试
    第一次上传，测试

---
+++ assets/script/GameController.ts
@@ -0,0 +1,50 @@
+// Learn TypeScript:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/typescript.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
+
+const {ccclass, property} = cc._decorator;
+
+@ccclass
+export default class NewClass extends cc.Component {
+
+    @property(cc.Label)
+    label: cc.Label = null;
+
+    @property
+    text: string = 'hello';
+
+    @property(cc.Node)
+    nodes: cc.Node[] = [];
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {}
+
+    start () {
+
+    }
+
+    private onShowNode0() {
+    	this.nodes[0].active = true;
+    }
+
+	private onHideNode0() {
+		this.nodes[0].active = false;
+    }
+
+	private onShowNode1() {
+    	this.node[1].active = true;
+    }
+
+	private onHideNode1() {
+		this.node[1].active = false;
+    }
+
+    // update (dt) {}
+}
---
+++ library/imports/6c/6c831baa-209d-40f9-8e7c-3a4aa925c15f.js
@@ -0,0 +1,58 @@
+"use strict";
+cc._RF.push(module, '6c831uqIJ1A+Y58OkqpJcFf', 'GameController');
+// script/GameController.ts
+
+// Learn TypeScript:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/typescript.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
+Object.defineProperty(exports, "__esModule", { value: true });
+var _a = cc._decorator, ccclass = _a.ccclass, property = _a.property;
+var NewClass = /** @class */ (function (_super) {
+    __extends(NewClass, _super);
+    function NewClass() {
+        var _this = _super !== null && _super.apply(this, arguments) || this;
+        _this.label = null;
+        _this.text = 'hello';
+        _this.nodes = [];
+        return _this;
+        // update (dt) {}
+    }
+    // LIFE-CYCLE CALLBACKS:
+    // onLoad () {}
+    NewClass.prototype.start = function () {
+    };
+    NewClass.prototype.onShowNode0 = function () {
+        this.nodes[0].active = true;
+    };
+    NewClass.prototype.onHideNode0 = function () {
+        this.nodes[0].active = false;
+    };
+    NewClass.prototype.onShowNode1 = function () {
+        this.node[1].active = true;
+    };
+    NewClass.prototype.onHideNode1 = function () {
+        this.node[1].active = false;
+    };
+    __decorate([
+        property(cc.Label)
+    ], NewClass.prototype, "label", void 0);
+    __decorate([
+        property
+    ], NewClass.prototype, "text", void 0);
+    __decorate([
+        property(cc.Node)
+    ], NewClass.prototype, "nodes", void 0);
+    NewClass = __decorate([
+        ccclass
+    ], NewClass);
+    return NewClass;
+}(cc.Component));
+exports.default = NewClass;
+
+cc._RF.pop();
